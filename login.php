929788c0983c8e42600a9aa94602cd2a02e00aa3
alextenney <alexandra.tenney@ucalgary.ca>
2020-04-03 00:41:34 -0600

    sql injection homepage challenge added

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+$username = $_POST['uname'];
+$password = $_POST['passw'];
+
+
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname = "minictf";
+
+$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+if (mysqli_connect_error()){
+    echo "This was sad";
+} else {
+    $SELECT = "SELECT * FROM users WHERE username='" .$username ."' AND password='" .$password ."'";
+
+    $stmt = $conn->prepare($SELECT);
+    echo $conn->error;
+    $stmt->execute();
+    $stmt->store_result();
+    $num_row = $stmt->num_rows;
+    $stmt->fetch();
+    $stmt->close();
+  
+      if( $num_row === 1 ) {
+        header("Location: ./search2.html");
+      } else {
+      header("Location: ./results.html");
+      }
+
+}
+
+?>
